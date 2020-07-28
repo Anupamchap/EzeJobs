b1aec2a2984fba441e17da471869db0a712dc9d4
Anupam Chaplot <39041794+Anupamchap@users.noreply.github.com>
2020-07-28 10:53:12 +0530

    Update store.js

--- src/store.js
+++ src/store.js
@@ -7,6 +7,7 @@ import reducer from './reducer';
 import { routerMiddleware } from 'react-router-redux'
 import createHistory from 'history/createBrowserHistory';
 
+
 export const history = createHistory();
 
 // Build the middleware for intercepting and dispatching navigation actions
